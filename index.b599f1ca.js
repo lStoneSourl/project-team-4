7fbdd8db23f7a25261df2db202225b4769328a4d
lStoneSourl <113469849+lStoneSourl@users.noreply.github.com>
2022-12-18 21:18:28 +0000

    Deploying to gh-pages from @ lStoneSourl/project-team-4@ce6dcdc3917bc620a5bb6a6903e6fb50fe20b43d 🚀

--- index.b599f1ca.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{spaceBetween:30,centeredSlides:!0,autoplay:{delay:2500,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.b599f1ca.js.map

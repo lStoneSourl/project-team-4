0e5976d2481de334161d1af8385fe15ead4229c4
lStoneSourl <113469849+lStoneSourl@users.noreply.github.com>
2022-12-20 09:26:59 +0000

    Deploying to gh-pages from @ lStoneSourl/project-team-4@f310d067ed81cb30403bd45f5e6c9a4a87a38620 🚀

---
+++ index.5ba75043.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal7-open7]"),closeModalBtn:document.querySelector("[data-modal7-close7]"),modal:document.querySelector("[data-modal7]")};function o(){e.modal.classList.toggle("is-hidden7")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.5ba75043.js.map
---
+++ index.dc1b7b45.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal8-open8]"),closeModalBtn:document.querySelector("[data-modal8-close8]"),modal:document.querySelector("[data-modal8]")};function o(){e.modal.classList.toggle("is-hidden8")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.dc1b7b45.js.map

fcd264b9d491e111e00e0085faa80baf2b4ebe11
Nickylood <110985074+Nickylood@users.noreply.github.com>
2022-12-22 17:40:55 +0100

    newcommit

---
+++ src/js/slick.js
@@ -0,0 +1,289 @@
+$('.rewiews__item').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    autoplay: false,
+    arrows: false,
+    asNavFor: '.rewiews__item2, .rewiews__item3',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $('.rewiews__item2').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: false,
+    asNavFor: '.rewiews__item, .rewiews__item3',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $('.rewiews__item3').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    appendDots: '.slick-dotees',
+    autoplay: false,
+    asNavFor: '.rewiews__item2, .rewiews__item',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  
+  $('.ingridient__slider1').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    appendDots: '.dots1',
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  
+  $('.ingridient__slider2').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    dots: true,
+    arrow: false,
+    appendDots: '.dots2',
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $('.ingridient__slider3').slick({
+    centerMode: false,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    dots: true,
+    arrow: false,
+    appendDots: '.dots3',
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesPerRow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });

78e7743026b049da35e2824d7c8e3df9879d2cde
lStoneSourl <lstonesourl@gmail.com>
2022-12-22 16:27:09 +0200

    modal

---
+++ src/js/modal.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal9-open9]'),
+      closeModalBtn: document.querySelector('[data-modal9-close9]'),
+      modal: document.querySelector('[data-modal9]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal9);
+    refs.closeModalBtn.addEventListener('click', toggleModal9);
+  
+    function toggleModal9() {
+      refs.modal.classList.toggle('is-hidden9');
+    }
+  })();
+  
